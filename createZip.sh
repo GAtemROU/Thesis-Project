50a08842757003ed4a2bb0557ff06d5e314e0043
tymy00001 <tymy00001@stud.uni-saarland.de>
2024-06-21 11:55:05 +0200

    fixed bugs, import works now, added createZip file

---
+++ createZip.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd RefGameShapesGazeFeedbackExperiment
+zip -r RefGameShapesGazeFeedbackExperiment.zip .
+cd ..
+rm RefGameShapesGazeFeedbackExperiment.zip
+mv RefGameShapesGazeFeedbackExperiment/RefGameShapesGazeFeedbackExperiment.zip RefGameShapesGazeFeedbackExperiment.zip
+
