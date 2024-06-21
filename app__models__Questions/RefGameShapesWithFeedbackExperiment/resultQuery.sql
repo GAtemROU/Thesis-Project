2e0c748a52039f457e79c9bc21cca4809d9408ea
tymy00001 <tymy00001@stud.uni-saarland.de>
2024-06-21 09:52:08 +0200

    initial commit

---
+++ app__models__Questions/RefGameShapesWithFeedbackExperiment/resultQuery.sql
@@ -0,0 +1,7 @@
+SELECT fileName, listNumber, assignmentId, hitId, workerId, origin, timestamp, partId, questionId, answer::TEXT, (data->>'itemid') as itemid, (data->>'itemType') as itemType, (data->>'msg') as msg, (data->>'img1') as img1, (data->>'img2') as img2, (data->>'img3') as img3, id FROM (
+	(SELECT * FROM Results WHERE experimentType='RefGameShapesWithFeedbackExperiment') as tmp1
+	LEFT OUTER JOIN Questions USING (QuestionId)
+	LEFT OUTER JOIN Groups USING (PartId)
+) as tmp
+WHERE LingoExpModelId = 1661
+ORDER BY partId, questionId, workerId
