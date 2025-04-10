1d24dcc5fad75f221a7050dba4717f7011cf9b4e
tymy00001 <tymy00001@stud.uni-saarland.de>
2025-04-10 20:51:06 +0200

    update: results chapter by adding more plots and the label section. update the final conclusion

--- analysis/plots.r
+++ analysis/plots.r
@@ -1,5 +1,29 @@
 library(GGally)
+library(tidyverse)
+library(scales)
 
-df <- read.csv("analysis/data/final_datasets/final_experiment_participants.csv")
+bysubject <- read.csv("analysis/data/final_datasets/final_experiment_participants_extended.csv")
 
-GGally::ggpairs(df, columns = 2:28)
+accuracybins <- bysubject %>%
+  mutate(simple_bin = round(AnswerAccuracySimple, 1),
+         complex_bin = round(AnswerAccuracyComplex, 1)) %>%
+  group_by(simple_bin, complex_bin) %>%
+  summarize(count = n())
+
+binplot <- ggplot(accuracybins) +
+  aes(x = simple_bin, y = complex_bin, size = count) +
+  geom_hline(yintercept = .5, linewidth = .2) +
+  geom_vline(xintercept = .5, linewidth = .2) +
+  geom_point(shape = 19) +
+  theme_bw() +
+xlim(0, 1) + ylim(0, 1) +
+scale_y_continuous(labels = scales::percent, breaks = seq(0, 1, by = 0.1)) +
+  scale_x_continuous(labels = scales::percent, n.breaks = 10) +
+  scale_y_continuous(labels = scales::percent, n.breaks = 10) +
+  labs(x = "Proportion of target choices (simple)",
+       y = "Proportion of target choices (complex)",
+       size = "Number of\nparticipants",
+       title = "By-participant performance")
+binplot
+
+ggsave("mdplot.png", width = 6, height = 4, units = "in", dpi = 800)
